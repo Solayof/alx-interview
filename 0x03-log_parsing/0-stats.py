bd55fb6985f403115407242fe87722ee6c058db0
Moses Solomon Ayofemi <solomonayofemi@gmail.com>
2024-06-18 02:13:07 +0100

    task 0 updated

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,72 +1,49 @@
 #!/usr/bin/python3
-"""
-Log parser
-"""
-import signal
-import sys
-import re
-from typing import Tuple
+"""A script that reads stdin line by line and computes metrics."""
+from contextlib import suppress
+from sys import stdin
+from typing import Dict, List, Tuple
+
 
 if __name__ == '__main__':
+    StatusCodes = Dict[int, int]
+    FileSize = List[int]
 
-    stats = {}
+    def _print_stats(file_size: FileSize, status_codes: StatusCodes) -> None:
+        """Private method to print stats."""
+        print('File size: {}'.format(file_size[0]))
+        for key, val in status_codes.items():
+            if val != 0:
+                print('{}: {}'.format(key, val))
 
-    def calc_stats(match: Tuple) -> None:
-        """
-        Processes and stores the stats
-        Returns:
-            None
-        """
-        global stats
+    def _parse_line(
+        line: str,
+        status_codes: StatusCodes,
+        file_size: FileSize
+            ) -> Tuple[StatusCodes, FileSize]:
+        """A private method to parse lines."""
+        with suppress(BaseException):
+            line = line[:-1]
+            word = line.split(' ')
+            file_size[0] += int(word[-1])
+            status_code = int(word[-2])
+            if status_code in status_codes:
+                status_codes[status_code] += 1
+        return status_codes, file_size
 
-        stats['size'] = stats.get('size', 0) + int(match[4])
-        stats[match[3]] = stats.get(match[3], 0) + 1
-
-    def display() -> None:
-        """
-        Displays the processed stats
-        Returns:
-            None
-        """
-        global stats
-        print(f"File size: {stats.get('size')}")
-
-        for stat in sorted(stats.keys()):
-            if stat == "size":
-                continue
-            print(f"{stat}: {stats.get(stat)}")
-
-    def handler(signum, frame):
-        """
-        Handler for Ctrl + C SIGINT
-        Returns:
-            None
-        """
-        display()
-
-    # listens to Ctrl + C then displays the stats
-    signal.signal(signal.SIGINT, handler)
-
-    # pattern to match for precessing
-    half = r'(\d+\.\d+\.\d+\.\d+) - (\[\d+-\d+-\d+ \d+:\d+:\d+\.\d+\])'
-
-    half2 = r' ("GET /projects/260 HTTP/1.1") (\d{3}) (\d{1,4})'
-
-    input_format = half + half2
-
-    # listens to the output from another script
-    count = 1
-    for line in sys.stdin:
-        match = re.search(input_format, line)
-        if match:
-            # processes and stores the stats
-            calc_stats(match.groups())
-        else:
-            pass
-
-        if count % 10 == 0:
-            # displays the stats if 10 lines has been processed
-            display()
-        count += 1
-
-    display()
+    file_size = [0]
+    status_codes = {200: 0, 301: 0, 400: 0, 401: 0,
+                    403: 0, 404: 0, 405: 0, 500: 0}
+    line_number = 1
+    try:
+        for line in stdin:
+            status_codes, file_size = _parse_line(
+                line, status_codes, file_size
+                )
+            if line_number % 10 == 0:
+                _print_stats(file_size, status_codes)
+            line_number += 1
+    except KeyboardInterrupt:
+        _print_stats(file_size, status_codes)
+        raise
+    _print_stats(file_size, status_codes)
